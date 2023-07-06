fcd64c1df76e8f4fb9aaaf6af18782e52d19fa55
Ruma303 <matteoh8@gmail.com>
2023-07-06 14:40:43 +0200

    Inizio react props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import secondCardImage from './img/amirr-zolfaqari-HDh6sSBxjQI-unsplash.jpg'
 const MyComponent = (props) => {
     console.log(props);
     console.log(props.name);
     console.log(props.age);
     return (
     <>
-        <h2>Hello, my name is {props.name}. </h2>
+        <h2>{'Hello'.toUpperCase()}, my name is {props.name}.</h2>
         <p> I am {props.age} years old.</p>
-        <img src={props.img}/>
+        <img
+            src={props.img}
+            className="img-class"
+            alt={`Foto di ${props.name}`}
+        />
     </>
     )
 }
@@ -15,7 +20,7 @@ function App() {
   return (
     <div className="App">
         <h1>Studiamo le Props</h1>
-        {/* <MyComponent name="Mario" age={35} />
+        {/* <MyComponent name="Mario" age={30 + 5} />
         <MyComponent name="Luisa" age={29} /> */}
         <MyComponent
             name={firstCard.name}
@@ -36,12 +41,12 @@ export default App;
 const firstCard = {
     name: "Mario",
     age: 35,
-    img: "https://unsplash.com/it/foto/J4zZNfjbppE"
+    img: "https://images.unsplash.com/photo-1497316730643-415fac54a2af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=928&q=80"
 }
 const secondCard = {
     name: "Luisa",
     age: 29,
-    img: "img/amirr-zolfaqari-HDh6sSBxjQI-unsplash.jpg"
+    img: secondCardImage
 }
 
 //appunti img e alt
