2b75976245ffc9913341f36787c0d417fb6aa0c4
Ruma303 <matteoh8@gmail.com>
2023-07-06 13:53:45 +0200

    aggiunta img

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+const MyComponent = (props) => {
+    console.log(props);
+    console.log(props.name);
+    console.log(props.age);
+    return (
+    <>
+        <h2>Hello, my name is {props.name}. </h2>
+        <p> I am {props.age} years old.</p>
+        <img src={props.img}/>
+    </>
+    )
+}
 function App() {
+    {/* { console.log(props) } */}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Studiamo le Props</h1>
+        {/* <MyComponent name="Mario" age={35} />
+        <MyComponent name="Luisa" age={29} /> */}
+        <MyComponent
+            name={firstCard.name}
+            age={firstCard.age}
+            img={firstCard.img}
+        />
+        <MyComponent
+            name={secondCard.name}
+            age={secondCard.age}
+            img={secondCard.img}
+        />
     </div>
   );
 }
 
 export default App;
+
+const firstCard = {
+    name: "Mario",
+    age: 35,
+    img: "https://unsplash.com/it/foto/J4zZNfjbppE"
+}
+const secondCard = {
+    name: "Luisa",
+    age: 29,
+    img: "img/amirr-zolfaqari-HDh6sSBxjQI-unsplash.jpg"
+}
+
+//appunti img e alt
